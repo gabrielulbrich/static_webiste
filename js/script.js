3a8b1c782a8c8786176c37bf54ff7a167dd1d0dd
Gabriel <gabriel_ulbrich@outlook.com>
2021-06-04 18:56:26 -0300

    formulario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-console.log('test');
+document.getElementById("send-button").addEventListener("click", function(event){
+    let nome = document.getElementsByName('nome')[0].value
+    let email = document.getElementsByName('email')[0].value
+    let telefone = document.getElementsByName('telefone')[0].value
+    let mensagem = document.getElementsByName('mensagem')[0].value
+    let aceito_wpp = document.getElementsByName('aceito_wpp')[0].checked
+    let aceito_email = document.getElementsByName('aceito_email')[0].checked
+    let termo = document.getElementsByName('termo')[0].checked
+
+    aceito_wpp = aceito_wpp == true ? '1' : '0';
+    aceito_email = aceito_email == true ? '1' : '0';
+    termo = termo == true ? '1' : '0';
+
+    let data = new FormData();
+
+    data.append('nome', nome)
+    data.append('email', email)
+    data.append('telefone',telefone)
+    data.append('mensagem', mensagem)
+    data.append('aceito_wpp', aceito_wpp)
+    data.append('aceito_email', aceito_email)
+    data.append('termo', termo)
+
+    // // Convert to a query string
+    let queryString = new URLSearchParams(data).toString();
+
+    console.log(queryString);
+
+    fetch('https://api.gabrielulbrich.com.br/gabrielulbrich/static-page', {
+    method: 'POST',
+    body: queryString,
+    headers: {
+        'Content-type': 'application/x-www-form-urlencoded'
+    }
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error);
+    });
+});
